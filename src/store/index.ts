0d756a8f70106a34f4f79785afb9753569e577be
sexnine <planz300@gmail.com>
2022-02-20 01:47:24 +1100

    init

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+export default defineStore("index", {
+  state: () => {
+    return {
+
+    }
+  },
+  getters: {},
+  actions: {}
+})
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      red: colors.red,
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+    }
+  },
+  plugins: [],
+}
