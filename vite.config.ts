00024fc8dbf145850e7f033754b3a3131e7a2415
sexnine <25860388+sexnine@users.noreply.github.com>
2022-06-30 17:52:01 +1000

    Add TS typing to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
   plugins: [vue()],
 });
