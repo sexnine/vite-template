d565cda303c789affa3e5b803160616c208b25f2
sexnine <25860388+sexnine@users.noreply.github.com>
2022-11-06 03:20:47 +1100

    Update deps, linting changes, tailwind config
    - Update most dependencies
    - Make changes to ESLint config, prettier formatting now done by ESLint
    - Tailwind config now includes all colors commented
    - Tailwind config now has typescript support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
 const colors = require("tailwindcss/colors");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     colors: {
-      red: colors.red,
-      gray: colors.gray,
       white: colors.white,
       black: colors.black,
+      // slate: colors.slate,
+      // gray: colors.gray,
+      // zinc: colors.zinc,
+      // neutral: colors.neutral,
+      // stone: colors.stone,
+      // red: colors.red,
+      // orange: colors.orange,
+      // amber: colors.amber,
+      // yellow: colors.yellow,
+      // lime: colors.lime,
+      // green: colors.green,
+      // emerald: colors.emerald,
+      // teal: colors.teal,
+      // cyan: colors.cyan,
+      // sky: colors.sky,
+      // blue: colors.blue,
+      // indigo: colors.indigo,
+      // violet: colors.violet,
+      // purple: colors.purple,
+      // fuchsia: colors.fuchsia,
+      // pink: colors.pink,
+      // rose: colors.rose,
     },
   },
   plugins: [],
